6dda895f4495bbcc39f4d37376fd31cb66858a55
Lucas Leao <lucasmelloleao@gmail.com>
2026-07-17 07:35:54 -0300

    lucas

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  typescript: {
+    // Ignora os erros de checagem de tipos do TypeScript apenas DURANTE o build
+    // Isso evita o overhead do compilador tsc travar a CPU da tua máquina
+    ignoreBuildErrors: true,
+  },
+
 };
 
 export default nextConfig;
